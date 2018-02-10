f403407dc1d9dd2298d236539e79b999a818ca0d
Hosam <hossmnasser938@gmail.com>
2018-02-10 17:36:57 +0200

    Prepare app for sending Email rather than displaying order summary

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,11 +12,9 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,15 +26,12 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     private int numberOfCoffees = 1;
-    private int numberOfOrders = 0;
-    LinearLayout primaryLinearLayout;
     Button orderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        primaryLinearLayout = (LinearLayout) findViewById(R.id.primary_linear_layout);
         orderButton = (Button) findViewById(R.id.order_button);
     }
 
@@ -44,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        numberOfOrders++;
         EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
         String name = nameEditText.getText().toString();
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check_box);
@@ -52,25 +46,14 @@ public class MainActivity extends AppCompatActivity {
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
         boolean hasChocolate = chocolateCheckBox.isChecked();
         String price = NumberFormat.getCurrencyInstance().format(calculatePrice(hasWhippedCream , hasChocolate));
-        primaryLinearLayout.addView(createNewTextView(createOrderSummary(name , price , hasWhippedCream , hasChocolate)) , primaryLinearLayout.getChildCount() - 1);
     }
 
-    /**
-     * This function creates a new text view
-     */
-    private TextView createNewTextView(String text){
-        TextView t = new TextView(this);
-        t.setText(text);
-        t.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT , ViewGroup.LayoutParams.WRAP_CONTENT));
-        t.setPadding((int)getResources().getDimension(R.dimen.justJavaPadding) , (int)getResources().getDimension(R.dimen.justJavaPadding) , 0 , 0);
-        return t;
-    }
 
     /**
      * This method returns summary of user order
      */
     private String createOrderSummary(String name , String price , boolean hasWhippedCream , boolean hasChocolate){
-        return "Order" + numberOfOrders + "Summary\n" + "\nName : " + name + "\nAdd whipped cream? "  + hasWhippedCream + "\nAdd chocolate? "  + hasChocolate + "\nQuantity : " + numberOfCoffees + "\nTotal : " + price + "\nThank You!\n---------";
+        return "Order" + "Summary\n" + "\nName : " + name + "\nAdd whipped cream? "  + hasWhippedCream + "\nAdd chocolate? "  + hasChocolate + "\nQuantity : " + numberOfCoffees + "\nTotal : " + price + "\nThank You!\n---------";
     }
 
 
